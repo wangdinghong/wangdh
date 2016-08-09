57c30f2a4db88b92865f19e234a28c63a17d236b
wangdinghong <18224010732@163.com>
2016-08-09 17:06:56 +0800

    git示范

---
+++ app/src/main/java/com/example/administrator/myapplication/MainActivity.java
@@ -0,0 +1,17 @@
+package com.example.administrator.myapplication;
+
+import android.os.PersistableBundle;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.Menu;
+
+import com.google.gson.Gson;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
